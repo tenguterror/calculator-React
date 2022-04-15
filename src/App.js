f441c9e66a4d2293882b1dd9e2a4244fb8a92421
Duard <vegasduard@gmail.com>
2022-04-14 19:47:49 -0600

    Formatting

--- src/App.js
+++ src/App.js
@@ -33,43 +33,43 @@ function reducer(state, { type, payload }) {
           ...state,
           operation: payload.operation,
           previousOperand: state.currentOperand,
-          currentOperand: null
-        }
+          currentOperand: null,
+        };
       }
 
       return {
         ...state,
         previousOperand: evaluate(state),
         operation: payload.operation,
-        currentOperand: null
-      }
+        currentOperand: null,
+      };
     case ACTIONS.CLEAR:
       return {};
   }
 }
 
 function evaluate({ currentOperand, previousOperand, operation }) {
-  const prev = parseFloat(previousOperand)
-  const current = parseFloat(currentOperand)
-  if (isNaN(prev) || isNaN(current)) return ""
-  let computation = ""
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if (isNaN(prev) || isNaN(current)) return '';
+  let computation = '';
   // eslint-disable-next-line default-case
   switch (operation) {
-    case "+":
-      computation = prev + current
-      break
-    case "-":
-      computation = prev - current
-      break
-    case "*":
-      computation = prev * current
-      break
-    case "/":
-      computation = prev / current
-      break
+    case '+':
+      computation = prev + current;
+      break;
+    case '-':
+      computation = prev - current;
+      break;
+    case '*':
+      computation = prev * current;
+      break;
+    case '/':
+      computation = prev / current;
+      break;
   }
 
-  return computation.toString()
+  return computation.toString();
 }
 
 function App() {
